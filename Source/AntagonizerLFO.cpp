727ecf03e47adf7ae9ea60bed9251f70249664b3
Colter Ulrich <cju5025@gmail.com>
2021-05-18 19:22:10 -0600

    adds LFO dsp

--- Source/AntagonizerLFO.cpp
+++ Source/AntagonizerLFO.cpp
@@ -1,1 +1,46 @@
 #include "AntagonizerLFO.h"
+#include "JuceHeader.h"
+
+LFO::LFO()
+{
+    reset();
+}
+
+LFO::~LFO()
+{
+    
+}
+
+void LFO::reset()
+{
+    mPhase = 0.f;
+    zeromem(mBuffer, sizeof(float) * maxBufferSize);
+}
+
+void LFO::setSampleRate(double sampleRate)
+{
+    mSampleRate = sampleRate;
+}
+
+void LFO::process(float inRate, float inDepth, int inNumSamples)
+{
+    const float rate = jmap(inRate, 0.f, 1.f, 0.01f, 10.f);
+    
+    for (int i = 0; i < inNumSamples; i++)
+    {
+        mPhase += rate / mSampleRate;
+        
+        if (mPhase > 1)
+        {
+            mPhase -= 1.f;
+        }
+        
+        const float lfoPosition = sinf(mPhase * TWO_PI) * inDepth;
+        mBuffer[i] = lfoPosition;
+    }
+}
+
+float* LFO::getBuffer()
+{
+    return mBuffer;
+}
--- Source/AntagonizerLFO.h
+++ Source/AntagonizerLFO.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "AudioHelpers.h"
+
+class LFO
+{
+public:
+    LFO();
+    ~LFO();
+    
+    void reset();
+    
+    void setSampleRate(double sampleRate);
+    void process(float rate, float depth, int numSamples);
+    
+    float* getBuffer();
+    
+private:
+    
+    double mSampleRate;
+    
+    float mPhase;
+    float mBuffer[maxBufferSize];
+};
